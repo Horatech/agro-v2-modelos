bed6c785d354e3cc6f4a0a4e9396995f50ce8415
Fernando Cortese <cortesefernando@gmail.com>
2022-08-16 11:37:39 -0300

    upd metadatos

--- src/mensajesLora/uplink/metadatos.ts
+++ src/mensajesLora/uplink/metadatos.ts
@@ -1,9 +1,9 @@
 export interface IMetadatos {
-  gatewayID: string;
-  timestamp: string;
-  rssi: number;
-  loRaSNR: number;
-  location: {
+  gatewayID?: string;
+  timestamp?: string;
+  rssi?: number;
+  snr?: number;
+  location?: {
     altitude: number;
     latitude: number;
     longitude: number;
