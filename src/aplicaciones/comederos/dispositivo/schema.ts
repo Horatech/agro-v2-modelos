af5c71bd943e7ab8ed8d4257f3a721f4111f8996
Fernando Cortese <cortesefernando@gmail.com>
2022-08-18 15:58:46 -0300

    ultima com

--- src/aplicaciones/comederos/dispositivo/schema.ts
+++ src/aplicaciones/comederos/dispositivo/schema.ts
@@ -2,4 +2,10 @@ export interface IDispositivoComedero {
   inicialMm?: number;
   alturaMaxima?: number;
   asignado?: boolean;
+  ultimaComunicacion?: {
+    fecha: string;
+    bateria: number;
+    cargando: boolean;
+    frecuenciaReporte: number;
+  };
 }
