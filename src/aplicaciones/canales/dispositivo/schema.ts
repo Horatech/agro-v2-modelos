6a51aa7725ce638b306a33f8c258c3bced6d7a46
Fernando Cortese <cortesefernando@gmail.com>
2022-08-19 09:38:43 -0300

    schema disp canal

--- src/aplicaciones/canales/dispositivo/schema.ts
+++ src/aplicaciones/canales/dispositivo/schema.ts
@@ -1,3 +1,9 @@
 export interface IDispositivoCanal {
   igm?: number;
+  ultimaComunicacion?: {
+    fecha: string;
+    bateria: number;
+    cargando: boolean;
+    frecuenciaReporte: number;
+  };
 }
