b85c8ba7ba5d07d247327b725ed43f84a1408129
Fernando Cortese <cortesefernando@gmail.com>
2022-08-26 11:55:08 -0300

    get device chirpstack

--- src/loraServers/chirpstack/device.dto.ts
+++ src/loraServers/chirpstack/device.dto.ts
@@ -12,3 +12,22 @@ export interface ICreateDeviceChirpstack {
     variables?: Record<string, string>;
   };
 }
+
+export interface IGetDeviceChirpstack {
+  device: {
+    applicationID: string;
+    description: string;
+    devEUI: string;
+    deviceProfileID: string;
+    isDisabled?: boolean;
+    name: string;
+    referenceAltitude?: number;
+    skipFCntCheck?: boolean;
+    tags?: Record<string, string>;
+    variables?: Record<string, string>;
+  };
+  deviceStatusBattery: number;
+  deviceStatusMargin: number;
+  lastSeenAt: string;
+  location: any;
+}
