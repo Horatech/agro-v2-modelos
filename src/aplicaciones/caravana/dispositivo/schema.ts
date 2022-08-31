659faa1bb8d4a79f9f3f4f422dddf49565528012
Fernando Cortese <cortesefernando@gmail.com>
2022-08-31 16:15:32 -0300

    opcional

--- src/aplicaciones/caravana/dispositivo/schema.ts
+++ src/aplicaciones/caravana/dispositivo/schema.ts
@@ -1,10 +1,10 @@
 export interface IDispositivoCaravana {
-  acelerometro: {
-    escala: string;
-    frecuencia: number;
-    freefallMg: number;
-    freefallMs: number;
-    wakeMg: number;
-    wakeMs: number;
+  acelerometro?: {
+    escala?: string;
+    frecuencia?: number;
+    freefallMg?: number;
+    freefallMs?: number;
+    wakeMg?: number;
+    wakeMs?: number;
   };
 }
