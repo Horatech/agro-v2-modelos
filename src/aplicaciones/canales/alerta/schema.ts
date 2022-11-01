5a2a9c61ec16aa0d82af23829c034d11df588620
Tatoh <tatukis@gmail.com>
2022-11-01 11:57:30 -0300

    up alertas

--- src/aplicaciones/canales/alerta/schema.ts
+++ src/aplicaciones/canales/alerta/schema.ts
@@ -1,6 +1,7 @@
+export type AlertaCanal = 'Error de lectura de sensor' | 'Error de config';
 export interface IAlertaCanal {
   fecha: string;
-  alerta?: 'Error de lectura de sensor';
+  alerta?: AlertaCanal;
   bateria?: number;
   cargando?: boolean;
   calidadDeLectura?: number;
