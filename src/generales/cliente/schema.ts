a67c69f4542eed668c8243a5c4582dd9a9d971f9
Tatoh <tatukis@gmail.com>
2022-12-28 10:34:43 -0300

    asd

--- src/generales/cliente/schema.ts
+++ src/generales/cliente/schema.ts
@@ -1,5 +1,5 @@
-import { TipoDispositivo } from "../tipoDispositivo";
-import { IImagenesCliente } from "./imagenes";
+import { TipoDispositivo } from '../tipoDispositivo';
+import { IImagenesCliente } from './imagenes';
 
 export interface ICliente {
   _id: string;
@@ -8,4 +8,28 @@ export interface ICliente {
   nombre: string;
   tiposDispositivo: TipoDispositivo[];
   imagenes?: IImagenesCliente;
+  configuraciones?: Configuraciones;
+}
+
+export interface Configuraciones {
+  establecimientos?: { colorLinea: string; colorRelleno: string };
+  lotes?: { colorLinea: string; colorRelleno: string };
+  animales?: {
+    colorDentro: string;
+    colorFueraLote: string;
+    colorFueraEst: string;
+    colorError: string;
+  };
+  canal?: { colorIcono: string; coloresNivel: string[] };
+  comederos?: { colorIcono: string; coloresNivel: string[] };
+  cisterna?: { colorIcono: string; coloresNivel: Semaforo[] };
+  sla?: { colorIcono: string; coloresNivel: Semaforo[] };
+  bombeo?: { colorIcono: string; colorIconoAlerta: string };
+}
+
+export interface Semaforo {
+  valorDesde: number;
+  valorHasta: number;
+  color: string;
+  etiqueta?: string;
 }
