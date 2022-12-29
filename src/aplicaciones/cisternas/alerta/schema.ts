bcf4d65547fdd3d44028f0d0c3e538ea1b9a8d46
Fernando Cortese <cortesefernando@gmail.com>
2022-12-29 12:25:41 -0300

    alerta bomba cisterna

--- src/aplicaciones/cisternas/alerta/schema.ts
+++ src/aplicaciones/cisternas/alerta/schema.ts
@@ -1,9 +1,9 @@
 export interface IAlertaNivelCisterna {
   fecha: string;
-  alerta?: 'Error de lectura de sensor';
+  alerta?: "Error de lectura de sensor";
 }
 
 export interface IAlertaBombaCisterna {
   fecha: string;
-  alerta?: 'Error de Downlink';
+  alerta?: string;
 }
