cdab7dabfd96d9866ec16f6ac92e171c6b7e6b7b
Fernando Cortese <cortesefernando@gmail.com>
2023-01-02 10:44:37 -0300

    index

--- src/aplicaciones/index.ts
+++ src/aplicaciones/index.ts
@@ -3,6 +3,6 @@ export * from "./canales";
 export * from "./caravana";
 export * from "./cisternas";
 export * from "./comederos";
-export * from "./estacionClima";
+export * from "./estacionMeteorologica";
 export * from "./represas";
 export * from "./sla";
