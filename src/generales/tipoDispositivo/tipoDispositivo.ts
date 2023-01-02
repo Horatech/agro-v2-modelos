1c0dd27b8cb035d38b7c55937c7aa15ddaf8ba1c
Fernando Cortese <cortesefernando@gmail.com>
2023-01-02 11:09:38 -0300

    tipo disp

--- src/generales/tipoDispositivo/tipoDispositivo.ts
+++ src/generales/tipoDispositivo/tipoDispositivo.ts
@@ -1,26 +1,28 @@
 export type TipoDispositivo =
+  | "Bomba de Cisterna"
   | "Caravana"
-  | "Lanza de Silobolsa"
-  | "Tracker de Silobolsa"
-  | "Nivel de Láminas de Agua"
-  | "Humedad de Suelo"
   | "Estacion de Bombeo"
-  | "Nivel de Comederos"
+  | "Estacion Meteorologica"
+  | "Humedad de Suelo"
+  | "Lanza de Silobolsa"
   | "Nivel de Canal"
-  | "Nivel de Represas"
   | "Nivel de Cisterna"
-  | "Bomba de Cisterna";
+  | "Nivel de Comederos"
+  | "Nivel de Láminas de Agua"
+  | "Nivel de Represas"
+  | "Tracker de Silobolsa";
 
 export const TIPOS_DISPOSITIVOS: TipoDispositivo[] = [
-  "Caravana",
-  "Lanza de Silobolsa",
-  "Tracker de Silobolsa",
-  "Nivel de Láminas de Agua",
-  "Humedad de Suelo",
-  "Estacion de Bombeo",
-  "Nivel de Comederos",
-  "Nivel de Canal",
-  "Nivel de Represas",
-  "Nivel de Cisterna",
   "Bomba de Cisterna",
+  "Caravana",
+  "Estacion de Bombeo",
+  "Estacion Meteorologica",
+  "Humedad de Suelo",
+  "Lanza de Silobolsa",
+  "Nivel de Canal",
+  "Nivel de Cisterna",
+  "Nivel de Comederos",
+  "Nivel de Láminas de Agua",
+  "Nivel de Represas",
+  "Tracker de Silobolsa",
 ];
