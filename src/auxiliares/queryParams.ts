ab750b15a23fb601e90eb798ed2f34f7c12999a8
Tatoh <tatukis@gmail.com>
2023-01-17 16:40:14 -0300

    Descripciones al queryParams

--- src/auxiliares/queryParams.ts
+++ src/auxiliares/queryParams.ts
@@ -1,10 +1,17 @@
 export interface IQueryParam {
+  // page: es el número de página a devolver de los resultados, comenzando desde cero.
   page?: string | number;
+  // limit: es la cantidad de resultados que debe devolver la consulta, cero para sin límite.
   limit?: string | number;
+  // sort: es el orden en que se devolverán los datos, es el nombre de una propiedad de la entidad, ej. ‘username’ para orden ascendente o ‘-username’ para orden descendente.
   sort?: string;
+  // filter: es un objeto con la misma estructura que recibe MongoDb para hacer las querys.
   filter?: string;
+  // populate: es un String u objeto con la misma estructura que recibe Mongoose.
   populate?: string;
+  // select: es un String que contiene separados por espacio todos los campos de la entidad que se desean obtener en la consulta, si no se proporciona se devuelve el objeto completo.
   select?: string;
+  // Se deja un campo genérico para agregar cualquier otro parámetro no contemplado.
   [key: string]: any;
 }
 
