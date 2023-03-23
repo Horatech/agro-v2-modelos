0818a59da0093e7f261c6557eae029a276066904
Fran <etchartfran1997@gmail.com>
2023-03-23 10:16:00 -0300

    joya

--- src/generales/gateway/update.ts
+++ src/generales/gateway/update.ts
@@ -1,6 +1,6 @@
 import { ICoordenadas } from "../../auxiliares/coordenadas";
 
 export interface IUpdateGateway {
-  gatewayEui: string;
-  ubicacion: ICoordenadas;
+  gatewayEui?: string;
+  ubicacion?: ICoordenadas;
 }
