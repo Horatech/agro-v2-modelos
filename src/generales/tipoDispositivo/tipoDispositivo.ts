cd5452c5f953c3ab6e827a28b022e60ac2c07a2c
Ignacio Martín Arano <tatukis@gmail.com>
2023-04-12 12:08:38 -0300

    tipo

--- src/generales/tipoDispositivo/tipoDispositivo.ts
+++ src/generales/tipoDispositivo/tipoDispositivo.ts
@@ -1,32 +1,34 @@
 export type TipoDispositivo =
-  | "Bomba de Cisterna"
-  | "Caravana"
-  | "Estacion de Bombeo"
-  | "Estacion Meteorologica"
-  | "Freatimetro"
-  | "Nivel de Canal"
-  | "Nivel de Cisterna"
-  | "Nivel de Comederos"
-  | "Nivel de Láminas de Agua"
-  | "Nivel de Represas"
-  | "Pluviometro"
-  | "Sensor de CO2"
-  | "Sensor Humedad de Suelo"
-  | "Tracker";
+  | 'Bomba de Cisterna'
+  | 'Caravana'
+  | 'Estacion de Bombeo'
+  | 'Estacion Meteorologica'
+  | 'Freatimetro'
+  | 'Nivel de Canal'
+  | 'Nivel de Cisterna'
+  | 'Nivel de Comederos'
+  | 'Nivel de Láminas de Agua'
+  | 'Nivel de Represas'
+  | 'Pluviometro'
+  | 'Sensor de CO2'
+  | 'Sensor Humedad de Suelo'
+  | 'Silobolsa'
+  | 'Tracker';
 
 export const TIPOS_DISPOSITIVOS: TipoDispositivo[] = [
-  "Bomba de Cisterna",
-  "Caravana",
-  "Estacion de Bombeo",
-  "Estacion Meteorologica",
-  "Freatimetro",
-  "Nivel de Canal",
-  "Nivel de Cisterna",
-  "Nivel de Comederos",
-  "Nivel de Láminas de Agua",
-  "Nivel de Represas",
-  "Pluviometro",
-  "Sensor de CO2",
-  "Sensor Humedad de Suelo",
-  "Tracker",
+  'Bomba de Cisterna',
+  'Caravana',
+  'Estacion de Bombeo',
+  'Estacion Meteorologica',
+  'Freatimetro',
+  'Nivel de Canal',
+  'Nivel de Cisterna',
+  'Nivel de Comederos',
+  'Nivel de Láminas de Agua',
+  'Nivel de Represas',
+  'Pluviometro',
+  'Sensor de CO2',
+  'Sensor Humedad de Suelo',
+  'Silobolsa',
+  'Tracker',
 ];
