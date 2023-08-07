3f28ee489ae4d4a8a7e414ec1f5dae92343b573a
Fernando Cortese <cortesefernando@gmail.com>
2023-08-07 10:47:57 -0300

    reporte arrancador

--- src/aplicaciones/bombeo-v2/reporteArrancador/schema.ts
+++ src/aplicaciones/bombeo-v2/reporteArrancador/schema.ts
@@ -1,5 +1,29 @@
 export interface IReporteBombeoArrancadorV2 {
+  // De reporte periodico
   dispositivo?: "Arrancador";
   numero?: number;
   estado?: "Encendido" | "Apagado" | "Encendido Manual";
+  habilitado?: boolean;
+  // De reporte especifico
+  nroMsg?: number;
+  on?: boolean;
+  corrienteAlimentacion?: number;
+  corrienteMotor?: number;
+  tensionAlimentacion?: number;
+  estadoSSW?: number;
+  factorPotencia?: number;
+  potenciaSalida?: number;
+  errorActual?: number;
+  alarmaActual?: number;
+  versionFirmware?: number;
+  corrienteFaseR?: number;
+  corrienteFaseS?: number;
+  corrienteFaseT?: number;
+  tensionFaseRS?: number;
+  tensionFaseST?: number;
+  tensionFaseTR?: number;
+  contHorasEnergizado?: number;
+  contHorasHabilitado?: number;
+  contHorasKWH?: number;
+  contHorasMWH?: number;
 }
