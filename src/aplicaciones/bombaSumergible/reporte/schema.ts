5f3100b1c1a9bbdb58add237fa0db82ac026ad07
Fernando Cortese <cortesefernando@gmail.com>
2023-09-05 14:50:15 -0300

    agregado modo de operacion

--- src/aplicaciones/bombaSumergible/reporte/schema.ts
+++ src/aplicaciones/bombaSumergible/reporte/schema.ts
@@ -1,5 +1,6 @@
 export interface IReporteBombaSumergible {
   estado?: "Encendido" | "Apagado";
+  modoOperacion?: "Started" | "Tripped 1" | "Tripped 2" | "Tripped 3";
   corrienteFaseR?: number;
   corrienteFaseS?: number;
   corrienteFaseT?: number;
