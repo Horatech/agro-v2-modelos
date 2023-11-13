e1c79a741f6d62f0043ccdcbd86704f50e5c7507
Fernando Cortese <cortesefernando@gmail.com>
2023-11-13 10:07:46 -0300

    represa

---
+++ src/aplicaciones/canales/represa/create.ts
@@ -0,0 +1,23 @@
+import { ConfiguracionCanal, IPolyline } from "../../..";
+
+export interface ICreateRepresa {
+  // Tentant
+  idCliente?: string;
+  idEstablecimiento?: string;
+  idLote?: string;
+  //
+  fechaCreacion?: string;
+  //deveui del canal
+  deveuiSensorCanal?: string;
+  // Algo de lo de bombeo
+  deveuiEstacionBombeo?: string;
+  numeroDeCanal?: number;
+  tipoDeCanal?: "Entrada" | "Salida";
+  //
+  nombre?: string;
+  descripcion?: string;
+  configuraciones?: ConfiguracionCanal;
+  igm?: number;
+  // Polyline
+  ubicacion?: IPolyline;
+}
---
+++ src/aplicaciones/canales/represa/update.ts
@@ -0,0 +1,29 @@
+import { IPolyline } from "../../../auxiliares";
+import { ConfiguracionCanal, IAlerta, IReporte } from "../../../generales";
+
+export interface IUpdateRepresa {
+  // Tentant
+  idCliente?: string;
+  idEstablecimiento?: string;
+  idLote?: string;
+  //
+  fechaCreacion?: string;
+  //deveui del canal
+  deveuiSensorCanal?: string;
+  // Algo de lo de bombeo
+  deveuiEstacionBombeo?: string;
+  numeroDeCanal?: number;
+  tipoDeCanal?: "Entrada" | "Salida";
+  //
+  nombre?: string;
+  descripcion?: string;
+  configuraciones?: ConfiguracionCanal;
+  igm?: number;
+  // Polyline
+  ubicacion?: IPolyline;
+  //
+  ultimaComunicacion?: string;
+  ultimoEstado?: "Ok" | "Alerta";
+  ultimaAlerta?: IAlerta;
+  ultimoReporte?: IReporte;
+}
