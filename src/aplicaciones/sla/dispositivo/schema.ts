5ddb0131e54e6468d95fac44d5294fdf19027c1c
Ignacio Martín Arano <tatukis@gmail.com>
2023-11-22 16:11:59 -0300

    Add cota and nombre properties to IDispositivoSLA interface

--- src/aplicaciones/sla/dispositivo/schema.ts
+++ src/aplicaciones/sla/dispositivo/schema.ts
@@ -2,6 +2,8 @@ import { Semaforo } from '../../../generales';
 
 export interface IDispositivoSLA {
   asignado?: boolean;
+  cota?: number;
+  nombre?: string;
   configuraciones?: {
     colorIcono: string | null;
     coloresNivel: Semaforo[] | null;
