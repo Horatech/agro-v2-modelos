de70f43900a382d7c33becc61b9a8ddce29f8dc2
Fernando Cortese <cortesefernando@gmail.com>
2024-07-24 14:07:24 -0300

    Update ambiente module schema and related files

--- src/generales/ambiente/schema.ts
+++ src/generales/ambiente/schema.ts
@@ -1,30 +1,59 @@
-import { IEstacionMeteorologica, IFreatimetro, IPluviometro, ISensorHumedadSuelo } from '../../aplicaciones';
-import { IUbicacion } from '../../auxiliares';
-import { ICliente } from '../cliente';
-import { IEstablecimiento } from '../establecimiento';
+import {
+  IEstacionMeteorologica,
+  IFreatimetro,
+  IPluviometro,
+  ISensorHumedadSuelo,
+} from "../../aplicaciones";
+import { IUbicacion } from "../../auxiliares";
+import { ICliente } from "../cliente";
+import { IEstablecimiento } from "../establecimiento";
 export interface IAmbiente {
   _id: string;
   nombre: string;
   ubicacion?: IUbicacion[];
   idCliente: string;
+  idEstablecimiento: string;
   fechaCreacion: string;
   configuraciones?: { colorLinea: string | null; colorRelleno: string | null };
   region?: string;
   tipoSuelo?: string;
   cultivo?: string;
   faseCultivo?: string;
-
-  sensoresSHS?: string[];
-  sensoresPluviometro?: string[];
-  sensoresFreatimetro?: string[];
-  sensoresMeteorologica?: string[];
-
-  //Populate
+  idSensorHumedadSuelos?: string[];
+  idPluviometros?: string[];
+  idFreatimetros?: string[];
+  idEstacionMeteorologicas?: string[];
+  // Virtuals
   establecimiento?: IEstablecimiento;
   cliente?: ICliente;
-  // Virtuals
-  dispositivoSHS?: ISensorHumedadSuelo[];
-  dispositivoPluviometro?: IPluviometro[];
-  dispositivoFreatimetro?: IFreatimetro[];
-  dispositivoMeteorologica?: IEstacionMeteorologica[];
+  sensorHumedadSuelos?: ISensorHumedadSuelo[];
+  pluviometros?: IPluviometro[];
+  freatimetros?: IFreatimetro[];
+  estacionMeteorologicas?: IEstacionMeteorologica[];
 }
+
+type OmitirCreate =
+  | "_id"
+  | "fechaCreacion"
+  | "establecimiento"
+  | "cliente"
+  | "sensorHumedadSuelos"
+  | "pluviometros"
+  | "freatimetros"
+  | "estacionMeteorologicas";
+
+export interface ICreateAmbiente
+  extends Omit<Partial<IAmbiente>, OmitirCreate> {}
+
+type OmitirUpdate =
+  | "_id"
+  | "fechaCreacion"
+  | "establecimiento"
+  | "cliente"
+  | "sensorHumedadSuelos"
+  | "pluviometros"
+  | "freatimetros"
+  | "estacionMeteorologicas";
+
+export interface IUpdateAmbiente
+  extends Omit<Partial<IAmbiente>, OmitirUpdate> {}
