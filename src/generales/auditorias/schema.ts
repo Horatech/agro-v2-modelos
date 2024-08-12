a0038f76efc1056a9320719be4a717fbec2252df
Nahuel Llamas <70917332+nahuellamas@users.noreply.github.com>
2024-08-12 14:32:50 -0300

    Update schema.ts

--- src/generales/auditorias/schema.ts
+++ src/generales/auditorias/schema.ts
@@ -11,38 +11,29 @@ export type TipoAction = "ADD" | "UPDATE" | "DELETE";
 export interface IAuditoria {
   _id: string;
   fechaCreacion: string;
-  action: TipoAction;
 
+  action: TipoAction;
   deveui: string;
   deviceName: string;
   appkey: string;
   conectividad: TipoConectividad;
   deviceProfileID?: string;
   tipo?: TipoDispositivo;
-
-  // Info de comunicacion
-  // snr?: number;
-  // rssi?: number;
-  // adr?: boolean;
-  // dr?: number;
   firmware?: string;
-  // ubicacion?: ICoordenadas | null;
 
   idUsuario: string;
   idCliente: string;
-  idEstablecimiento?: string;
   idTipoDispositivo: string;
   idLoteDispositivo?: string;
   idLoraServer?: string;
   // Virtuals
   usuario: IUsuario;
   cliente?: ICliente;
-  establecimiento?: IEstablecimiento;
   tipoDispositivo?: ITipoDispositivo;
   loteDispositivo?: ILoteDispositivo;
   loraServer?: ILoraServer;
 }
 
-type OmitirCreate = "_id" | "fechaCreacion";
+type OmitirCreate = "_id" | "fechaCreacion" | "usuario" | "cliente" | "tipoDispositivo" | "loteDispositivo" | "loraServer";
 
 export interface ICreateAuditoria extends Omit<Partial<IAuditoria>, OmitirCreate> { }
