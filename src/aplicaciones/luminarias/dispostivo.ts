b894474200be9e68fec49e48bea0fdce54423cee
Fernando Cortese <cortesefernando@gmail.com>
2024-11-26 15:01:25 -0300

    Agregar interfaz ISetEstrategiaCalendarioLuminaria con propiedades para control de dimmer

--- src/aplicaciones/luminarias/dispostivo.ts
+++ src/aplicaciones/luminarias/dispostivo.ts
@@ -8,6 +8,15 @@ export interface ISetMargenesAlarmaLuminaria {
   currentLowLimit?: number; // A
 }
 
+export interface ISetEstrategiaCalendarioLuminaria {
+  numeroEstrategia?: 1 | 2 | 3;
+  habilitada?: boolean;
+  horaDimmer1?: string; // 'HH:mm:ss'
+  dimmer1?: number; // Dimmer 0-100 // 0 = Apagado
+  horaDimmer2?: string; // 'HH:mm:ss'
+  dimmer2?: number; // Dimmer 0-100 // 0 = Apagado
+}
+
 export interface IDispositivoLuminaria {
   deviceWorkingTime?: number; // Minutos
   restartTimes?: number; // Veces
